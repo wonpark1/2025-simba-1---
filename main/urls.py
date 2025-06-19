c6a0e5476b3e240bba46cf8c4e1e3ebbead12c8f
hairyung2002 <hairyung2002@naver.com>
2025-06-19 21:40:43 +0900

    Feat:Commentary

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,7 @@ from . import views
 app_name = "main"
 urlpatterns = [
     path('', views.mainpage, name="mainpage"),
+    path('comment/', views.comentpage, name="comment_page"),
+    path('create/', create, name="create"),
+    path('update/<int:id>', update, name="update"),
 ]
