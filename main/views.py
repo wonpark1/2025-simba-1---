1202c89f405a5241c52c5a425ca50c41c3bafcbb
hairyung2002 <hairyung2002@naver.com>
2025-06-22 15:29:13 +0900

    Fix/Doyoun-DB

--- main/views.py
+++ main/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from .models import *
-import re
 from django.db.models import Count
 from datetime import datetime
 
