8bd61fccb484267407d6b26cda95fcfc7760e571
hairyung2002 <hairyung2002@naver.com>
2025-06-23 20:20:41 +0900

    Fix/Doyoun-MyCommentPage

--- users/views.py
+++ users/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import auth
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from main.views import pushpagestack, poppagestack, previouspage
 
 @login_required
 def mypage(request):
+    pushpagestack(request, request.get_full_path())
     user = request.user
     context = {
         'user':user,
@@ -13,10 +15,12 @@ def mypage(request):
     return render(request, 'users/mypage.html', context)
 
 def logout(request):
+
     auth.logout(request)
     return redirect('accounts:accountpage')
 
 def mycomment(request):
+    pushpagestack(request, request.get_full_path())
     user = request.user
     comments = user.comment_set.all()
     context = {
