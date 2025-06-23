4ee8d068c5685866b40688aae1a1b8d4e69041e5
hairyung2002 <hairyung2002@naver.com>
2025-06-23 23:25:24 +0900

    Fix/Doyoun-AllLookCard DB

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,20 @@ SECRET_KEY = 'django-insecure-!-Simbathon-Team1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "a3c5-210-94-220-230.ngrok-free.app",  # 현재 뜬 주소
+]
+
+# ⚡ CSRF_TRUSTED_ORIGINS
+CSRF_TRUSTED_ORIGINS = [
+    "https://a3c5-210-94-220-230.ngrok-free.app",
+    # Django 4.1+면 와일드카드도 OK
+    # "https://*.ngrok-free.app",
+]
+
+
 # Application definition
 
 INSTALLED_APPS = [
