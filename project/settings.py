b5b69565b0c1898048aed0e727ed13e83f9a9c2c
hairyung2002 <hairyung2002@naver.com>
2025-06-24 21:37:43 +0900

    Fix/LookCard_Stack

--- project/settings.py
+++ project/settings.py
@@ -16,15 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_HOSTS = [
-    "67ad-210-94-220-230.ngrok-free.app",
-    "localhost",
-    "127.0.0.1"
-]
-
-CSRF_TRUSTED_ORIGINS = [
-    "https://67ad-210-94-220-230.ngrok-free.app"
-]
+ALLOWED_HOSTS = ['*']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
